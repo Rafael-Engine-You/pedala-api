10e6e60b16b8672290c34e4bd072140a359ec8a2
Glêsio Santos <glesioss@gmail.com>
2026-07-07 08:39:13 -0300

    inicializando configuração do banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '102030',
+      database: 'pedaladb',
+      entities: [],
+      migrations: []
+    }),
+    UsuarioModule
+  ],
   providers: [AppService],
 })
 export class AppModule {}
