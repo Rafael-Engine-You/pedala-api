6e60e125aea55b3508d35454711fc0f6be316f4b
Glêsio Santos <glesioss@gmail.com>
2026-07-09 08:50:45 -0300

    criando modulo de estações

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { EstacoesModule } from './estacoes/estacoes.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { typeOrmConfig } from './config/typeorm.config';
       envFilePath: '.env'
     }),
     TypeOrmModule.forRoot(typeOrmConfig),
-    UsuarioModule
+    UsuarioModule,
+    EstacoesModule
   ],
   providers: [AppService]
 })
