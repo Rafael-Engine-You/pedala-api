765396f91c0a0e6d9edf05fb891ce4571f979c16
Glêsio Santos <glesioss@gmail.com>
2026-07-15 09:41:44 -0300

    ajuste no cadastro de marcas

--- src/marca/marca.controller.ts
+++ src/marca/marca.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
 import { MarcaService } from './marca.service';
 import { MarcaModel } from './marca.model';
 
@@ -17,5 +17,17 @@ export class MarcaController {
     @Get()
     async todasMarcas(): Promise<MarcaModel[]> {
         return this.marcaService.carregarMarcas()
+    }
+
+    @Put("/atualizar/:id")
+    @HttpCode(204)
+    async atualizarDadosDaMarca(@Param("id") marcaId: string, 
+            @Body() data: {nome: string}): Promise<void>{
+        await this.marcaService.atualizarMarca(marcaId, data)        
+    }
+
+    @Delete("/remover/:id")
+    async deletarMarca(@Param("id") id:string):Promise<void> {
+        await this.marcaService.removerMarca(id)
     }
 }
--- src/marca/marca.service.ts
+++ src/marca/marca.service.ts
@@ -15,9 +15,6 @@ export class MarcaService {
         const existeMarca = await this.marcaRepository.findOneBy({ nomeMarca: data.nome })
         if(existeMarca) throw new BadRequestException(`Marca já registrada com 
             este nome ${data.nome}`)
-
-        console.log('******', data.nome)
-
         const marca = this.marcaRepository.create({ nomeMarca: data.nome})
         await this.marcaRepository.save(marca)    
     }
@@ -34,5 +31,13 @@ export class MarcaService {
         })
         if(!marca) throw new BadRequestException("Marca não encontrada")
         return marca
+    }
+
+    async atualizarMarca(id: string, data: {nome:string}):Promise<void>{
+           await this.marcaRepository.update(id, {nomeMarca: data.nome}) 
+    }
+
+    async removerMarca(id:string): Promise<void> {
+        await this.marcaRepository.delete(id)
     }
 }
